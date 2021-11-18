c990242cf77ac0b53d6dffb00b71f2953275b9ab
contrerasjake <contreras.jaspherjake@gmail.com>
2021-11-18 12:00:03 +0800

    Added Register and Login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PagesController extends Controller
+class DashboardController extends Controller
 {
     public function index(){
-        return view('index');
+        return view('dashboard');
     }
 }
