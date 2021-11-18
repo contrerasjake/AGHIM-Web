e91bae4d3f9234ed49188874a0d7a059be10db82
millaresluis <millaresluis21@gmail.com>
2021-11-18 15:42:38 +0800

    Added Logout function & Navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,22 +1,83 @@
-<ul>
-    <li>
-        <a href="{{ route('home') }}">Home</a>
-    </li>
-    @auth
-        <li>
-            <a href="">Jake</a>
-        </li>
-        <li>
-            <a href="">Logout</a>
-        </li>
-    @endauth
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}" >
+   <title>Document</title>
+ </head>
+ <body>
+   <nav class="navbar">
+     <!-- LOGO -->
+     <div class="logo">AGHIM</div>
+     <!-- NAVIGATION MENU -->
+     <ul class="nav-links">
+       <!-- USING CHECKBOX HACK -->
+       <input type="checkbox" id="checkbox_toggle" />
+       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+       <!-- NAVIGATION MENUS -->
+       <div class="menu">
+         <li>
+            <a href="{{ route('home') }}">Home</a>
+         </li>
 
-    @guest
-        <li>
-            <a href="{{ route('login') }}">Login</a>
-        </li>
-        <li>
-            <a href="{{ route('register') }}">Register</a>
-        </li>
-    @endguest
-</ul>
+         <li>
+            <a href="">About Us</a>
+         </li>
+
+         <li>
+            <a href="">Contact Us</a>
+         </li>
+         
+        @auth
+        
+            
+            <li class="navbar_dropdown">
+                <a href="/">Services</a>
+                <!-- DROPDOWN MENU -->
+                <ul class="dropdown">
+                    <li><a href="/">Food Delivery </a></li>
+                    <li><a href="/">Package Delivery</a></li>
+                    <li><a href="/">Car Rental</a></li>
+                </ul>
+            </li>
+
+            
+
+            <li class="navbar_dropdown">
+                <a href="/">Avatar</a>
+                <!-- DROPDOWN MENU -->
+                <ul class="dropdown2">
+                    <!-- profile dropdown -->
+                    <li>
+                        <a href="">Profile</a>
+                    </li>
+                    <li>
+                        <a href="">Settings</a>
+                    </li>
+                    <li>
+                        @csrf
+                        <a href="{{ route('logout')}}" method="post">Logout</a>
+                    </li>
+                </ul>
+            </li>
+            
+        @endauth
+
+
+        @guest
+            <li>
+                <a href="{{ route('login') }}">Login</a>
+            </li>
+            <li>
+                <a href="{{ route('register') }}">Register</a>
+            </li>
+        @endguest
+
+         
+       </div>
+     </ul>
+   </nav>
+ </body>
+</html>
