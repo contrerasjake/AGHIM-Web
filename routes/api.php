3902746d5e7373c0457fa9e507f370e0b23c172b
millaresluis <millaresluis21@gmail.com>
2021-12-02 13:29:51 +0800

    Added restaurant model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RestaurantsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -16,4 +17,5 @@ Route::middleware('auth:api')->prefix('v1')->group(function(){
     Route::get('/user', function(Request $request){
         return $request->user();
     });
+    Route::get('/restaurants/{restaurant}', [RestaurantsController::class, 'show']);
 });
